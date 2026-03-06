f23f2a61707a0536c785d8aabe34a8955e94e98e
Tanvir Hassan Anik <tanvir.h.anik@gmail.com>
2026-03-06 15:27:41 +0600

    sign in form implementation

--- app/authentication/sign-in/page.tsx
+++ app/authentication/sign-in/page.tsx
@@ -1,8 +1,27 @@
 'use client'
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { UserLogin, loginSchema } from "../components/UserSchema";
 
-export default function SignIn(){
-    return (
-        <div>
-        </div>
-    )
+export default function LoginForm() {
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: zodResolver(loginSchema)
+  });
+
+  const onSubmit = (data: UserLogin) => {
+    console.log("Validated data:", data);
+    
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input type="email" {...register("email")} placeholder="Email" />
+      {errors.email && <p>{errors.email.message}</p>}
+
+      <input type="password" {...register("password")} placeholder="Password" />
+      {errors.password && <p>{errors.password.message}</p>}
+
+      <button type="submit">Login</button>
+    </form>
+  )
 }
